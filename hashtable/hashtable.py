54ac0064cfffd00aac00e165ded906af2bf0267c
Anthony K <antonyk@users.noreply.github.com>
2020-07-07 19:39:47 -0400

    implemented most of the hashtable with collision support

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -7,22 +7,91 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def find(self, key):
+        if self.key == key:
+            return (self, self.value)
+        else:
+            cur = self
+            while (cur.next):
+                cur = cur.next
+                if cur.key == key:
+                    return (cur, cur.value)
+            return None
+
+
+    def get(self, key):
+        # return the node matching with the matching key
+        found = self.find(key)
+        if (found):
+            (node, value) = found
+            return value
+        else:
+            return None
+
+
+    def put(self, key, value):
+        found = None
+        cur = self
+        if cur.key == key:
+            found = cur
+
+        while (found is None and cur.next):
+            cur = cur.next
+            if cur.key == key:
+                found = cur
+
+        if (found is not None):
+            found.value = value
+        else:
+            cur.next = HashTableEntry(key, value)
+
+    def delete(self, key):
+        # return a tuple of (deleted value, new head)
+        prev = None
+        cur = self
+        found = None
+        if cur.next is None:
+            if cur.key == key:
+                return (cur.value, None)
+            else:
+                raise 'No matching key found'
+
+        while (not found and cur.next):
+            prev = cur
+            cur = cur.next
+
+        if cur.key == key:
+            found = cur
+
+
+    def __len__(self):
+        result = 1
+        node = self
+        while (node.next):
+            result += 1
+            node = node.next
+        return result
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
+# fnvsize = 2**32
+# FNV_offset_basis = 14695981039346656037
+# FNV_prime = 1099511628211
 
 
 class HashTable:
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
-    Implement this.
     """
 
-    def __init__(self, capacity):
+    def __init__(self, capacity = MIN_CAPACITY):
+        if capacity < MIN_CAPACITY:
+            capacity = MIN_CAPACITY
         self.capacity = capacity
         self.data = [None] * capacity
+        self.total_keys = 0 # increment on insert
+        self.ops_since_recalc = 0
 
 
     def get_num_slots(self):
@@ -41,32 +110,42 @@ class HashTable:
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
-        Implement this.
         """
-        # Your code here
-        pass
+        return round(self.total_keys / self.capacity)
+        # total_keys = 0
+        # for item in self.data:
+        #     if item:
+        #         total_keys = total_keys + len(item)
+        # return round(total_keys / self.capacity)
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
-        Implement this, and/or DJB2.
         """
-
-        # Your code here
-        pass
+        FNV_offset_basis = 0x811c9dc5
+        FNV_prime = 0x01000193
+        result = FNV_offset_basis
+        if not isinstance(key, bytes):
+            key = key.encode("UTF-8", "ignore")
+        for b in key:
+            result = result * FNV_prime
+            # add this for a 32-bit hashing function
+            result &= 0xffffffffffffffff
+            result = result ^ b
+        return result
 
 
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
-        Implement this, and/or FNV-1.
         """
-        # Your code here
-        pass
+        # sbytes = key.encode()
+        result = 5381
+        for i in key:
+            result = (result * 33) + ord(i)
+            result &= 0xffffffff  # add this for a 32-bit hashing function
+        return result
 
 
     def hash_index(self, key):
@@ -80,14 +159,14 @@ class HashTable:
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
-        Implement this.
         """
-        # Your code here
-        pass
-
+        idx = self.hash_index(key)
+        slot = self.data[idx]
+        if slot:
+            slot.add(key, value)
+        else:
+            self.data[idx] = HashTableEntry(key, value)
 
     def delete(self, key):
         """
